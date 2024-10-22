bcf93a640d016b6e0f0b0b837cda2b796e5ce80e
Samoudi_Anas <72927816+SamoudiAnas@users.noreply.github.com>
2024-10-22 22:47:03 +0100

    fix database connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -12,14 +12,8 @@ const {
 /**
  * Create a new instance of Sequelize
  */
-const sequelize = new Sequelize({
+const sequelize = new Sequelize(process.env.DATABASE_PUBLIC_URL, {
   dialect: "postgres",
-  host: POSTGRES_DB_HOST,
-  port: POSTGRES_DB_PORT as unknown as number,
-  database: POSTGRES_DB_NAME,
-  username: POSTGRES_DB_USER,
-  password: POSTGRES_DB_PASSWORD,
-  logging: NODE_ENV === "development" ? console.log : false,
+  logging: process.env.NODE_ENV === "development" ? console.log : false,
 });
-
 export { sequelize };
