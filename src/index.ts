8de7992aa829be02d252db03e10ab30f0061304b
Samoudi_Anas <72927816+SamoudiAnas@users.noreply.github.com>
2024-10-23 22:01:39 +0100

    add methods in cors

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,7 @@ app.use(
   cors({
     origin: ["http://localhost:3000", "https://dental-clinic-user.vercel.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 app.use(express.json());
